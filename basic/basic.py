1b56798ddf629b1ad3c75867d856954f59739bb2
om gajera <omgajera8@gmail.com>
2026-06-27 02:00:34 +0530

    Add initial implementation of basic arithmetic operations and age input handling

---
+++ basic/basic.py
@@ -0,0 +1,11 @@
+# text = input("just tyoe your age: ")
+# age = int(text)
+# cgpa = 7.8
+# print(type(age))
+# print(type(text))
+
+
+a = int(input("enter first number : "))
+b = int(input("enter second number : "))
+c = a + b;
+print (c, type(c))
---
+++ basic/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+age = int(text)
+# cgpa = 7.8
+# print(type(age))
+# print(type(text))
