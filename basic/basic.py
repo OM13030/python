8ddd9966a99abdde3fe8182e5e4a649c310f3ffc
om gajera <omgajera8@gmail.com>
2026-06-27 15:34:46 +0530

    Refactor arithmetic operations and add conditionals file

--- basic/basic.py
+++ basic/basic.py
@@ -9,3 +9,8 @@ a = int(input("enter first number : "))
 b = int(input("enter second number : "))
 c = a + b;
 print (c, type(c))
+
+print("a = b ; ", a+b)
+print("a / b : ", a/b)
+print("a // b: ", a//b)
+print("a ** b : ", a ** b)
--- basic/tempCodeRunnerFile.py
+++ basic/tempCodeRunnerFile.py
@@ -1,4 +1,4 @@
-age = int(text)
-# cgpa = 7.8
-# print(type(age))
-# print(type(text))
+a = int(input("enter first number : "))
+# b = int(input("enter second number : "))
+# c = a + b;
+# print (c, type(c))
