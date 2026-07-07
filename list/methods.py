4d0708e0be7aec49a32a4bfa235fb889a08ddf7c
om gajera <omgajera8@gmail.com>
2026-07-07 18:10:19 +0530

    Add list comprehension example and enhance list methods with additional operations

--- list/methods.py
+++ list/methods.py
@@ -1,3 +1,12 @@
 marks = [2,34,6,34,23]
+extra_marks = [12,23,45]
 marks.append(63) # this will will add the 63 to original list
 print(marks)
+marks.pop()  # delete thge last element
+marks.insert(1,7)  # 7 will add before index 1
+marks.extend(extra_marks)
+print(marks)
+marks.sort()
+
+
+
